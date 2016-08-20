216cc2762853e0610e2fd6ee47668c12a7c656a7
mmaletin <maletinme@gmail.com>
2016-08-20 18:02:29 +0500

    Readme attribute demo project

---
+++ Assets/Scripts/ReadmeAttribute.cs
@@ -0,0 +1,81 @@
+﻿
+// Made by Max Maletin https://github.com/mmaletin
+
+using UnityEngine;
+using System;
+
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+/// <summary>
+/// This attribute creates a collapsable text area in the inspector. Use it on serializable variables. Unity RTF tags are supprted.
+/// </summary>
+[AttributeUsage(AttributeTargets.Field | AttributeTargets.Property, Inherited = false, AllowMultiple = true)]
+public class ReadmeAttribute : PropertyAttribute {
+
+    public string name;
+    public string value;
+
+    public bool selectable;
+
+    public ReadmeAttribute(string value, bool selectable = false) : this("<b>Readme</b>", value, selectable) { }
+
+    public ReadmeAttribute(string name, string value, bool selectable = false)
+    {
+        this.name = name;
+        this.value = value;
+
+        this.selectable = selectable;
+    }
+}
+
+
+#if UNITY_EDITOR
+[CustomPropertyDrawer(typeof(ReadmeAttribute))]
+public class DecoratorReadmeDrawer : DecoratorDrawer
+{
+    private static GUIStyle invisibleButtonStyle = new GUIStyle();
+    private static GUIStyle textAreaStyle = new GUIStyle(GUI.skin.textArea) { richText = true, padding = new RectOffset(10, 10, 10, 10) };
+    private static GUIStyle headerStyle = new GUIStyle(EditorStyles.foldout) { richText = true };
+
+    bool groupIsVisible = false;
+    float readmeHeight = 0;
+
+    override public void OnGUI(Rect position)
+    {
+        var readme = attribute as ReadmeAttribute;
+
+        // Readme foldout //
+
+        Rect foldoutLabelRect = new Rect(position.xMin, position.yMin, position.width, readme.selectable ? 16f : GetHeight());
+
+        //Invisible button to catch text clicks
+        if (GUI.Button(foldoutLabelRect, "", invisibleButtonStyle))
+        {
+            groupIsVisible = !groupIsVisible;
+        }
+
+        // Readme text //
+
+        groupIsVisible = EditorGUI.Foldout(foldoutLabelRect, groupIsVisible, readme.name, headerStyle);
+
+        if (groupIsVisible)
+        {
+            GUIContent readmeText = new GUIContent(readme.value);
+
+            readmeHeight = textAreaStyle.CalcHeight(readmeText, position.width);
+
+            Rect textRect = new Rect(position.xMin, position.yMin + 18, position.width, readmeHeight);
+
+            GUI.TextArea(textRect, readme.value, textAreaStyle);
+        }
+    }
+
+    //Used by Unity to determine element size
+    override public float GetHeight()
+    {
+        return groupIsVisible ? (20f + readmeHeight) : 18f;
+    }
+}
+#endif
---
+++ Assets/Scripts/ReadmeAttributeSample.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+public class ReadmeAttributeSample : MonoBehaviour {
+
+    [Readme("<b>Readme (En)</b>", "Readme attribute allows you to add a description for a script or a variable to Unity inspector.\n\nIt supports Unity RTF tags and allows you to <b>highlight</b> the most crucial part of a description. Foldout arrow allows you to collapse the text so it won't take a lot of space.\n\nYou can only use this attribute <color=#A00000FF>on a serializable variable</color>, the same way you would use Space or Header attributes. Multiple instances of an attribute are allowed.")]
+    [Readme("<b>Readme (Ru)</b>", "Атрибут Readme добавляет описание для скрипта или переменной в инспектор Unity.\n\nПоддержка тэгов RTF редактора Unity позволяет <b>обратить внимание</b> пользователей на особо важные части описания, при этом текст описания можно свернуть, чтобы он не занимал много места.\n\nАтрибут можно использовать <color=#A00000FF>только на сериализуемых переменных</color>, как и атрибуты Unity вроде Space и Header. Допускается несколько атрибутов для одной переменной.")]
+
+    public int answer = 42;
+}
